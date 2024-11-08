786f648f830a255d78bb1372bf29d033cd3e4c6c
Ilma Gusinac <ilma.gusinac@stu.ibu.edu.ba>
2024-11-08 11:03:13 +0100

    Updated MainActivity

--- app/src/main/java/com/example/mindfulmate/MainActivity.kt
+++ app/src/main/java/com/example/mindfulmate/MainActivity.kt
@@ -7,43 +7,47 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import com.example.mindfulmate.ui.theme.MindfulMateTheme
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.mindfulmate.presentation.navigation.NavGraph
+import com.example.mindfulmate.presentation.navigation.Screen
+import com.example.mindfulmate.presentation.theme.MindfulMateTheme
+import com.example.mindfulmate.presentation.view_model.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    lateinit var navController: NavHostController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
         enableEdgeToEdge()
         setContent {
             MindfulMateTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                val mainViewModel: MainViewModel = hiltViewModel()
+
+                navController = rememberNavController()
+
+                Scaffold(
+                    content = { innerPadding ->
+                        NavGraph(
+                            navController = navController,
+                            startDestination = if (mainViewModel.isUserSignedIn()) Screen.Home.route else Screen.Welcome.route,
+                            modifier = Modifier.padding(innerPadding)
+                        )
+                    }
+                )
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MindfulMateTheme {
-        Greeting("Android")
     }
 }
