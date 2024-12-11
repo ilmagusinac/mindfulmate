ff02ecffddadfee1c3fe1277a5626f2fd2d30882
ilmagusinac <ilma.gusinac@stu.ibu.edu.ba>
2024-12-11 13:56:17 +0100

    Added additional Implementations

---
+++ app/src/main/java/com/example/mindfulmate/presentation/ui/screen/profile/component/profile/ProfileRowSection.kt
@@ -0,0 +1,105 @@
+package com.example.mindfulmate.presentation.ui.screen.profile.component.profile
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.mindfulmate.R
+import com.example.mindfulmate.presentation.theme.DuskyBlue
+import com.example.mindfulmate.presentation.theme.Grey
+import com.example.mindfulmate.presentation.theme.LightGrey
+import com.example.mindfulmate.presentation.theme.MindfulMateTheme
+import com.example.mindfulmate.presentation.ui.screen.settings.component.IconPlacement
+
+@Composable
+fun ProfileRowSection(
+    title: String,
+    label: String,
+    onRowClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    @DrawableRes placeholderRes: Int = R.drawable.ic_profile,
+    tint: Color = DuskyBlue
+) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .shadow(
+                elevation = dimensionResource(id = R.dimen.elevation_medium),
+                shape = RoundedCornerShape(dimensionResource(id = R.dimen.rounded_corners_small)),
+                clip = false
+            )
+            .clip(RoundedCornerShape(dimensionResource(id = R.dimen.rounded_corners_small)))
+            .background(Color.White)
+            .clickable { onRowClick() }
+            .padding(
+                horizontal = dimensionResource(id = R.dimen.padding_medium),
+                vertical = dimensionResource(id = R.dimen.padding_xsmall)
+            )
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            IconPlacement(
+                placeholderRes = placeholderRes,
+                tint = tint
+            )
+            Column(
+                horizontalAlignment = Alignment.Start,
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_medium))
+            ) {
+                Text(
+                    text = label,
+                    style = MaterialTheme.typography.bodyMedium.copy(
+                        color = LightGrey,
+                        fontSize = 10.sp
+                    )
+                )
+                Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.border_thin)))
+                Text(
+                    text = title,
+                    style = MaterialTheme.typography.titleMedium.copy(
+                        color = Grey,
+                        fontSize = 14.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                )
+
+            }
+        }
+    }
+}
+
+@Preview(
+    backgroundColor = 0xFF4CB5E3,
+    showBackground = true
+)
+@Composable
+private fun ProfileRowSectionPreview() {
+    MindfulMateTheme {
+        ProfileRowSection(
+            title = "Get your emotional analytics",
+            label = "If you didn't do it already you can now!",
+            onRowClick = {}
+        )
+    }
+}
